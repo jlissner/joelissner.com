eb238abbd0ea75ddc65fd08b835b8618714c1226
jlissner <jlissner@gmail.com>
2017-06-15 21:59:36 -0500

    update football page

--- src/pages/football/football.js
+++ src/pages/football/football.js
@@ -1,13 +1,51 @@
 export class Football {
+	constructor() {
+		this.editGameDetails = true;
+		this.game = {};
+		this.drives = [];
+		this.plays = [];
+		this.comp = false;
+		this.down = 1;
+		this.play = '';
+		this.player = '';
+		this.yardage = 0;
+		this.defPlay = '';
+		this.defPlayer = '';
+	}
+
+	toggleEditMode() {
+		this.editGameDetails = !this.editGameDetails;
+	}
+
+	addPlay() {
+		this.plays.push({
+			comp: this.comp,
+			down: this.down,
+			play: this.play,
+			player: this.player,
+			yardage: this.yardage,
+			defPlay: this.defPlay,
+			defPlayer: this.defPlayer
+		});
+
+		this.comp = false;
+		this.down = (this.yardage > 10 || parseInt(this.down, 10) + 1 > 4) ? 1 : parseInt(this.down, 10) + 1;
+		this.play = '';
+		this.player = '';
+		this.yardage = 0;
+		this.defPlay = '';
+		this.defPlayer = '';
+	}
+
 	teams = [
-		{id: 0, name: 'Jacksonville', qb: 'Allan'},
-		{id: 1, name: 'Houston', qb: 'Lock'}
+		{id: 10, name: 'Jacksonville', qb: 'Allan'},
+		{id: 3, name: 'Houston', qb: 'Lock'}
 	];
 
 	game = {
 		id: 0,
-		home: 0,
-		away: 1,
+		home: 10,
+		away: 3,
 		homeScore: 20,
 		awayScore: 21
 	}
@@ -16,7 +54,7 @@ export class Football {
 		{
 			game: 0,
 			id: 0,
-			offense: 0
+			offense: true // true for home team, false for away team
 		}
 	];
 
